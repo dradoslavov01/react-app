4537725f64873f81d368d9eaa9da6720a9c8d0ce
dradoslavov01 <daniradoslavov01@abv.bg>
2021-04-08 15:54:07 +0300

    Created Navigation

---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,33 @@
+import style from './Header.module.css';
+import NavigationItem from '../NavigationItem/NavigationItem';
+
+const items = [
+    { id: "1", content: 'Going to 1' },
+    { id: "2", content: 'Going to 2' },
+    { id: "3", content: 'Going to 3' },
+    { id: "4", content: 'Going to 4' },
+    { id: "5", content: 'Going to 5' },
+    { id: "6", content: 'Going to 6' },
+    { id: "7", content: 'Going to 7' },
+    { id: "8", content: 'Going to 8' },
+    { id: "9", content: 'Going to 9' },
+    { id: "10", content: 'Going to 10' }
+
+];
+
+const Header = () => {
+    return (
+        <nav className={style.navigation}>
+            <ul>
+                <li className={style.listItem}><img src="/white-origami-bird.png" alt="white origami" /></li>
+                {items.map(i => {
+                    return (
+                        <NavigationItem key={i.id}>{i.content}</NavigationItem>
+                    )
+                })}
+            </ul>
+        </nav >
+    );
+}
+
+export default Header;
--- client/src/services/bookService.js
+++
@@ -1,5 +0,0 @@
-export function getAll() {
-   return fetch('http://localhost:3000/books')
-        .then(res => res.json())
-        .catch(err => alert(err))
-}
